f9bfee141db80a0797e16211c6d62f86e8140af7
QiuJH <q19931101uu@gmail.com>
2020-05-26 13:23:12 +0800

    feat: init

---
+++ index.js
@@ -0,0 +1,92 @@
+const express = require('express')
+const app = express()
+
+const cors = require('cors')
+app.use(cors())
+
+app.use(express.json())
+
+const morgan = require('morgan')
+app.use(morgan((tokens, req, res) => {
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms',
+        JSON.stringify(req.body)
+    ].join(' ')
+}))
+
+let persons = [
+    {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+    },
+    {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+    },
+    {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+    },
+    {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": 4
+    }
+]
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+    const text = `Phonebook has info for ${persons.length} people. <br /> <p>${new Date()}</p>`
+    res.send(text)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(p => p.id === id)
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(p => p.id !== id)
+
+    res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+    const person = { ...req.body, id: Math.floor(Math.random() * 10000) }
+
+    if (person.name && person.number && persons.every(p => p.name !== person.name)) {
+        persons.push(person)
+
+        return res.json(person)
+    }
+
+    if (!persons.every(p => p.name !== person.name)) {
+        return res.status(403).send({ error: 'name must be unique!' })
+    }
+
+    if (person.name || person.number) {
+        return res.status(403).send({ error: `name and number can't be empty!` })
+    }
+
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
